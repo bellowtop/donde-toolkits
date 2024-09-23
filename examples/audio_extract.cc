867c81e610349951b03ab78ac665abfa113f32a0
Seki Rocc <sekirocc@gmail.com>
2024-09-23 20:56:50 +0800

    update

--- examples/audio_extract.cc
+++ examples/audio_extract.cc
@@ -11,6 +11,11 @@ using donde_toolkits::audio_process::FFmpegAudioProcessor;
 int main(int argc, char** argv) {
     FFmpegAudioProcessor v{};
 
+    if (argc <= 1) {
+        std::cerr << "usage: ./audio_extract path_to_mp4_file";
+        return 1;
+    }
+
     std::string video_filepath = argv[1];
     std::string audio_filepath = std::filesystem::path(video_filepath).replace_extension(".wav").string();
 
